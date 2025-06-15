838972d233f2e162e6f8ada3543bae76d7fd54a8
kurushimee <jermacoff@gmail.com>
2025-06-15 14:57:41 +0700

    add animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
     
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
@@ -24,7 +26,13 @@ public class Player : MonoBehaviour {
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * (moveSpeed * Time.deltaTime);
 
+        isWalking = moveDir != Vector3.zero;
+        
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
